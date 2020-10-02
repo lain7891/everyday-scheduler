65d012f62789c13a8f8715426a5d419ea59ab2ba
Estefania <nialupo87@gmail.com>
2020-10-02 18:59:02 -0400

    created timeblocks

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     console.log("Hello World");
+
+
+ // DOM VARIABLES
+
+
+// JS VARIABLES
+
+
+// FUNCTION DEFINITIONS
+
+
+// FUNCTION CALLS
+
+
+// EVENT LISTENER
 });
+
